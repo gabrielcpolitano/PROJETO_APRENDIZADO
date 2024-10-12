bce2d684330e52eae7f380728eb33a1285693aae
gabrielcpolitano <gabriel.cpolitano@gmail.com>
2024-10-12 15:40:02 +0200

    Thanks God - desafio 1

---
+++ Aprendendo - Inglês/1- Como Aprender/desafio1.js
@@ -0,0 +1,9 @@
+function segredo() {
+  console.log('1- Escolha Qual Assento vai Falar: Americano, Britânico...')
+  console.log('2- Aprender sobre Fónetica a forma que se fala as palavras.')
+  console.log('3- Aprenda o Padrão Básico de formar frases.')
+  console.log('4- Leia, Escreva, Aprenda novas Palavras, Escute em Inglês, Fale com Nativos')
+  console.log('5- Converse com um Nátivo do assento que está aprendendo. (Pode ser o Chat-GPT)')
+}
+
+segredo()
---
+++ Aprendendo - Inglês/2- Fonética/desafio1.js
@@ -0,0 +1,8 @@
+function a(){
+  console.log('A no alfabeto tem o som de [ ei ]')
+  console.log('A nas palavras tem som de [ é ]')
+  console.log('Alligator -> [ Éélegueitor ]')
+  console.log('Ant -> [ Énti ]')
+}
+
+a()
