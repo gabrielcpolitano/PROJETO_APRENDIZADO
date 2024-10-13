bbbd01b91964bf0078b17e8efb0d632df30931ba
gabrielcpolitano <gabriel.cpolitano@gmail.com>
2024-10-13 10:58:52 +0200

    Thanks God - (GIT)desafio3.js

---
+++ Aprendendo - Git/1- Fundamentos/desafio3.js
@@ -0,0 +1,15 @@
+function ondeEstou(){
+  console.log('Para o Git mostrar em que branch ele está: [ git branch ]')
+}
+
+function novaAmbiente(){
+  console.log('Para o Git criar uma nova branch: [ git branch nome_nova_branch ]')
+}
+
+function mudançaAmbiente(){
+  console.log('Para o Git mudar para outra branch existente: [ git switch nome_existente_branch ]')
+}
+
+ondeEstou()
+novaAmbiente()
+mudançaAmbiente()
