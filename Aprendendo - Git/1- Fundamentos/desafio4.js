b7bd0a2845bdf9486b9bd7947eb5836d1cae75df
gabrielcpolitano <gabriel.cpolitano@gmail.com>
2024-10-13 11:01:49 +0200

    Thanks God - (GIT)desafio4.js

---
+++ Aprendendo - Git/1- Fundamentos/desafio4.js
@@ -0,0 +1,6 @@
+function prática(){
+  console.log('1- Cria uma branch')
+  console.log('2- Entra nela')
+  console.log('3- Faz uma modificação e salva nessa branch')
+  console.log('1- Muda para branch principal e observa o que aconteceu')
+}
