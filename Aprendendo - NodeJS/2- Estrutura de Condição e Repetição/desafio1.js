353250eaee4db24d796854cd8ce4a5a4c580216b
gabrielcpolitano <gabriel.cpolitano@gmail.com>
2024-10-13 18:17:33 +0200

    Thanks God - (Condição e Repetição)- desafio1 com teste

---
+++ Aprendendo - NodeJS/2- Estrutura de Condição e Repetição/desafio1.js
@@ -0,0 +1,35 @@
+function médiaAritmética(nota1, nota2, nota3){
+  if(typeof nota1 !== 'number' || typeof nota2 !== 'number' || typeof nota3 !== 'number'){
+    console.log('NOTA INVÁLIDA')
+    return
+  }
+
+
+  const média = (nota1 + nota2 + nota3) / 3
+
+  if(média >= 7 && média <= 10){
+    console.log('EXCELENTE!!')
+  
+  } else if(média >= 3 && média < 7){
+    console.log('DÁ PARA MELHORAR')
+
+  } else if(média >= 0 && média < 3){
+    console.log('HORRÍVEL')
+
+  } else{
+    console.log('NOTA INVÁLIDA')
+  }
+}
+
+// Testes
+médiaAritmética(8, 9, 10);  // EXCELENTE!!
+médiaAritmética(5, 6, 4);   // DÁ PARA MELHORAR
+médiaAritmética(1, 2, 0);   // HORRÍVEL
+médiaAritmética(11, 10, 12);// NOTA INVÁLIDA
+médiaAritmética(-1, 4, 5);  // NOTA INVÁLIDA
+médiaAritmética(3, 5, 's'); // NOTA INVÁLIDA
+médiaAritmética(7, 7, 7);   // EXCELENTE!!
+médiaAritmática(3, 3, 3);   // DÁ PARA MELHORAR
+médiaAritmática(0, 0, 0);   // HORRÍVEL
+médiaAritmática(10, 10, 10);// EXCELENTE!!
+médiaAritmática(0, 3, 0);   // DÁ PARA MELHORAR
