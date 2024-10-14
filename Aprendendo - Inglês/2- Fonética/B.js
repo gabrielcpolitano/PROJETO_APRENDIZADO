9c10609ed0e2a3476d74dbfe97611f33bba8316d
gabrielcpolitano <gabriel.cpolitano@gmail.com>
2024-10-14 15:58:02 +0200

    Thanks God - Fonética B

---
+++ Aprendendo - Inglês/2- Fonética/B.js
@@ -0,0 +1,9 @@
+const b = () => {
+  console.log('Book: búk')
+  console.log('Bicycle: báy-sih-kuhl')
+  console.log('Beach: bítch')
+  console.log('Butterfly: bút-er-flai')
+  console.log('Balloon: bə-lún')
+}
+
+b()
