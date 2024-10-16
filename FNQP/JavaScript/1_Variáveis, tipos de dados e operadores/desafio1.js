f7f8aee1edc93c0eb68e64d714be29d30dcd6cbc
gabrielcpolitano <gabriel.cpolitano@gmail.com>
2024-10-16 11:23:29 +0200

    Thanks God - FNQP: DESAFIO 1

---
+++ FNQP/JavaScript/1_Variáveis, tipos de dados e operadores/desafio1.js
@@ -0,0 +1,10 @@
+// 1. Declaração de Variáveis
+// Declare três variáveis com valores diferentes. Uma string, um número e um booleano. Em seguida, exiba cada valor no console.
+
+const declaração = () => {
+  console.log('Olá')
+  console.log(5)
+  console.log(true)
+}
+
+declaração()
