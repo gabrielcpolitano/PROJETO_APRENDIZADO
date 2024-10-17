85d488cb08744d2c474b9487a70f607ef1b77716
gabrielcpolitano <gabriel.cpolitano@gmail.com>
2024-10-17 18:40:32 +0200

    Thanks God - Desafio Fibonacci

---
+++ Aprendendo - NodeJS/2- Estrutura de Condição e Repetição/desafio13.js
@@ -0,0 +1,22 @@
+// FUP que solicite a quantidade de termos e imprima fibonacci até termo solicitado;
+
+//1, 1, 2, 3, 5, 8,
+
+let soma = 1
+
+function fibonacci(number){
+
+  for(let i = 1; i <= number; i++ ){
+
+    
+    console.log(i)
+    
+    soma = i
+
+  console.log(soma)
+
+}
+}
+
+
+fibonacci(2)
