7e2d9b30d93be5de40c391300c123506664dafe7
gabrielcpolitano <gabriel.cpolitano@gmail.com>
2024-10-18 16:47:53 +0200

    Thanks God - Desafio 13

--- Aprendendo - NodeJS/2- Estrutura de Condição e Repetição/desafio13.js
+++ Aprendendo - NodeJS/2- Estrutura de Condição e Repetição/desafio13.js
@@ -1,22 +1,35 @@
 // FUP que solicite a quantidade de termos e imprima fibonacci até termo solicitado;
+function fibonacc(x){
+  let anterior = 0 // recebe anterior número da sequência
+  let atual = 1 // recebe atual número da sequência
+  let proximo = Infinity // quanta a anterior
 
-//1, 1, 2, 3, 5, 8,
+  for(let i = 0; i < x; i++){
+    proximo = anterior + atual
+    anterior = atual
+    atual = proximo
+  }
 
-let soma = 1
-
-function fibonacci(number){
-
-  for(let i = 1; i <= number; i++ ){
-
-    
-    console.log(i)
-    
-    soma = i
-
-  console.log(soma)
-
-}
+  return anterior
 }
 
+console.log(fibonacc(10))
 
-fibonacci(2)
+/*
+function fibonacci(num) {
+  if (num <= 1) return num
+
+  let total = 1
+  let previous = 0
+
+  for (let i = 2; i <= num; i++) {
+    total += previous
+    previous = total - previous
+  }
+
+  return total
+}
+
+console.log(fibonacci(10))
+
+*/
