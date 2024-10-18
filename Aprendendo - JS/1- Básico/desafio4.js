25b730e6e1bcec22612f403be4fcfdd721134710
gabrielcpolitano <gabriel.cpolitano@gmail.com>
2024-10-18 14:12:56 +0200

    Thanks God - Desafio 4

---
+++ Aprendendo - JS/1- Básico/desafio4.js
@@ -0,0 +1,10 @@
+// FUP com função de seta, que soma dois números.
+
+const soma = (a, b) => a + b
+
+// Testes
+console.log(soma(2, 3)); // Esperado: 5
+console.log(soma(-1, 4)); // Esperado: 3
+console.log(soma(0, 0)); // Esperado: 0
+console.log(soma(5.5, 4.5)); // Esperado: 10
+console.log(soma(-5, -7)); // Esperado: -12
