1b974e8e853b8f8d024b350891d99f034f522727
gabrielcpolitano <gabriel.cpolitano@gmail.com>
2024-10-18 14:30:36 +0200

    Thanks God - Desafio 6

---
+++ Aprendendo - JS/1- Básico/desafio6.js
@@ -0,0 +1,9 @@
+// FUP que adiciona + 1 toda vez que for chamado a um contador.
+const add = número => número + 1
+
+// Testes
+console.log(add(0));  // Esperado: 1
+console.log(add(10)); // Esperado: 11
+console.log(add(-1)); // Esperado: 0
+console.log(add(99)); // Esperado: 100
+console.log(add(5.5)); // Esperado: 6.5
