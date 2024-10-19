92d579115ee89c9af8701615007c1a0640266a93
gabrielcpolitano <gabriel.cpolitano@gmail.com>
2024-10-19 14:44:20 +0200

    Thanks God - Desafio 12

--- Aprendendo - NodeJS/2- Estrutura de Condição e Repetição/desafio12.js
+++ Aprendendo - NodeJS/2- Estrutura de Condição e Repetição/desafio12.js
@@ -2,26 +2,42 @@
 
 
 function conta(numero) {
-  let contar = 0
+  let impar = 0
+  let par = 0
 
-  for(let i = 0; i < numero; i++){
+  for(let i = 0; i <= numero; i++){
     if(i % 2 === 0){
-      contar++
+      impar -= i
+    
+    }else{
+      par += i
     }
   }
-  return contar
+  return{ impar, par}
 }
 
-// Testes manuais
-console.log("Teste 1 - até 10:");
+console.log(conta(10))
+
+// Teste 1 - até 10
 let resultado1 = conta(10);
-console.log(resultado1 === 5 ? "Passou!" : "Falhou, esperado: 5, recebido: " + resultado1);
+console.log("Teste 1 - até 10");
+console.log(resultado1.par === -30 ? "Passou!" : "Falhou! Esperado subtração: -30, Recebido: " + resultado1.par);
+console.log(resultado1.impar === 25 ? "Passou!" : "Falhou! Esperado soma: 25, Recebido: " + resultado1.impar);
 
-console.log("Teste 2 - até 20:");
-let resultado2 = conta(20);
-console.log(resultado2 === 10 ? "Passou!" : "Falhou, esperado: 10, recebido: " + resultado2);
+// Teste 2 - até 5
+let resultado2 = conta(5);
+console.log("Teste 2 - até 5");
+console.log(resultado2.par === -6 ? "Passou!" : "Falhou! Esperado subtração: -6, Recebido: " + resultado2.par);
+console.log(resultado2.impar === 9 ? "Passou!" : "Falhou! Esperado soma: 9, Recebido: " + resultado2.impar);
 
-console.log("Teste 3 - até 0:");
-let resultado3 = conta(0);
-console.log(resultado3 === 0 ? "Passou!" : "Falhou, esperado: 0, recebido: " + resultado3);
+// Teste 3 - até 100
+let resultado3 = conta(100);
+console.log("Teste 3 - até 100");
+console.log(resultado3.par === -2550 ? "Passou!" : "Falhou! Esperado subtração: -2550, Recebido: " + resultado3.par);
+console.log(resultado3.impar === 2500 ? "Passou!" : "Falhou! Esperado soma: 2500, Recebido: " + resultado3.impar);
 
+// Teste 4 - até 0
+let resultado4 = conta(0);
+console.log("Teste 4 - até 0");
+console.log(resultado4.par === 0 ? "Passou!" : "Falhou! Esperado subtração: 0, Recebido: " + resultado4.par);
+console.log(resultado4.impar === 0 ? "Passou!" : "Falhou! Esperado soma: 0, Recebido: " + resultado4.impar);
