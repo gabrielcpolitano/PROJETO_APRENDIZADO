adcd808a2cec2e7be0bfe567b56543d7d2d836db
gabrielcpolitano <gabriel.cpolitano@gmail.com>
2024-10-19 18:47:48 +0200

    Thanks God - English

---
+++ Aprendendo - Inglês/3- Palavras/10.js
@@ -0,0 +1,23 @@
+// struggle -> luta
+
+// despite -> apesar de
+
+// seem -> parecer
+
+// themselves -> eles mesmo
+
+// uncovers -> descobre
+
+// were -> eram
+
+// accurate -> preciso
+
+// median -> mediana
+
+// mean -> média
+
+// disco -> discotéca
+
+// mode -> modo
+
+// occurs -> ocorre
